ef3419eca1ecd195250a7b10ef752d60f32e2292
proto <hunde@kind.de>
2025-12-31 23:13:09 +0100

    removed old files from root dir

--- test-import.js
+++
@@ -1,6 +0,0 @@
-import * as spriteExtractor from './src/spriteExtractor.js';
-
-console.log('Module:', spriteExtractor);
-console.log('Keys:', Object.keys(spriteExtractor));
-console.log('extractAllPokemonSpritesToPNG:', spriteExtractor.extractAllPokemonSpritesToPNG);
-console.log('extractPokemonSprite:', spriteExtractor.extractPokemonSprite);
--- test-import2.mjs
+++
@@ -1,9 +0,0 @@
-try {
-    const module = await import('./src/spriteExtractor.js');
-    console.log('Module loaded successfully');
-    console.log('Keys:', Object.keys(module));
-} catch (error) {
-    console.error('Error loading module:');
-    console.error(error);
-    console.error(error.stack);
-}
